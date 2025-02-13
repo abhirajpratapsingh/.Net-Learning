c456b8aa8b4ab8b574fdbdfdf199cb72c4f8bee7
Abhiraj Pratap Singh <abhirajpratapsingh13@gmail.com>
2025-02-14 04:06:13 +0530

    Differences (Asp.net webForms vs MVC vs Asp.net Core)

---
+++ Section 01-Introduction/2. WebForms [vs] Asp.Net Mvc [vs] Asp dotNet Core.cs
@@ -0,0 +1,38 @@
+
+/*
+
+
+-----------------------------------------
+Asp.Net Web Forms
+=> 2002
+=> Performace issues due to server events and view-state
+=> Unit testing difficult
+=> mainly supports windows-only on the host
+=> Not cloud-friendly
+=> Not open-source
+=> Event-driven development model. ( if user click on the button it offers click event on the server )
+
+
+---------------------------------------------
+Asp.Net MVC
+=> 2009
+=> Model views controller
+=> performance issues due to some dependencies with asp.net (.net framework)
+=> lack of support of cross-platform
+=> mainly supports windows-only on the host
+=> Slightly cloud-friendly
+=> Open source
+=> Model-view-controller pattern 
+
+
+-------------------------------------------
+Asp.Net Core
+=> 2016
+=> Faster performance
+=> Cross-platform support
+=> Cloud-friendly
+=> Open source
+=> Model-view-controller patterns
+
+
+--------------------------------------------------------
