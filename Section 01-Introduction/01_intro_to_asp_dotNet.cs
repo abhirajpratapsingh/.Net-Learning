c4d1635f1fcf1cae04f33a352191dd952ca39b8f
Abhiraj Pratap Singh <abhirajpratapsingh13@gmail.com>
2025-02-14 03:12:58 +0530

    Introduction of .Net Core

---
+++ Section 01-Introduction/01_intro_to_asp_dotNet.cs
@@ -0,0 +1,21 @@
+
+/*
+Asp.Net core is a cross-platform, high-preformance, 
+open-source framework for building web applications and services. 
+
+cross-platform => Asp.Net core apps can be hosted on Windows, Linux and Mac.
+
+Can be hosted on different servers => supports Kestrel, IIS , Nginx, Docker, Apache.
+
+Open-Source
+
+Cloud-enabled => Out of box support for microsoft Azure
+
+There are total four parts of Asp.Net core:
+1: Asp.Net core MVC
+2: Asp.Net core Web API
+3: Asp.Net core Razor Pages
+4: Asp.Net Core Blazor
+
+
+*/
